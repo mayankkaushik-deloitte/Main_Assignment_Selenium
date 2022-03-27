b6f3590895e23e4bb33b8b76e470d1ccd61d0ea9
Mayank Kaushik <mayakaushik187@gmail.com>
2022-03-27 14:34:04 +0530

    Added xlsx file

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -1,2 +1,18 @@
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class BaseClass {
+    public static WebDriver driver;
+    public static XSSFWorkbook wbook;
+    public static XSSFSheet sheet;
+    private final String FILEPATH = "C:\\Users\\mayakaushik\\m_AS_4\\src\\test\\java\\excel.xlsx";
+    @BeforeTest
+    public void DataSetUp() throws IOException{
+        FileInputStream fis = new FileInputStream(FILEPATH);
+    }
 }
