a55013ba6eb783988ce37d0619f35481bd7d2050
Mayank Kaushik <mayakaushik187@gmail.com>
2022-03-28 23:03:56 +0530

    Added the Login Page FindBy xpaths

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,2 +1,15 @@
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends BaseClass{
+    //Page Factory- OR;
+    @FindBy(xpath = "/html/body/div/div/div[2]/div/div[1]/div[2]/button")
+    WebElement bankmanager;
+
+    @FindBy(xpath = "//button[text()='Customer Login']")
+    WebElement cutomer;
+    //initialize page objects
+    
+
 }
