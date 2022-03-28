2d635f3c35a4806a20d926290bef41c3bd81de62
Mayank Kaushik <mayakaushik187@gmail.com>
2022-03-29 00:19:42 +0530

    Added Base class init methods

--- src/main/java/BaseClass.java
+++ src/main/java/BaseClass.java
@@ -4,36 +4,38 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class BaseClass {
     public static WebDriver driver;
-    public static XSSFWorkbook wbook;
-    public static XSSFSheet sheet;
-    private final String FILEPATH = "C:\\Users\\mayakaushik\\m_AS_4\\src\\test\\java\\Excel.xlsx";
-    public void DataSetUp() throws IOException{
-        FileInputStream fis = new FileInputStream(FILEPATH);
-        wbook = new XSSFWorkbook(fis);
-        sheet = wbook.getSheet("Sheet1");
+    public static Properties prop;
+    public BaseClass(){
+        try{
+            prop = new Properties();
+            FileInputStream ip=new FileInputStream("C:\\Users\\mayakaushik\\Main_Assignment_Selenium\\src\\data\\Excel.xlsx");
+            prop.load(ip);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    public static void addCustomer() throws InterruptedException {
-        AddCustomer.addCustomerFun();
-    }
-    public void DataTearDown() throws IOException {
-        wbook.close();
-    }
-    public static void SetUp(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mayakaushik\\Desktop\\selenium\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+
+    public static void init(){
+        String browserName=prop.getProperty("browser");
+
+        if(browserName.equals("chrome")){
+            System.setProperty("webdriver.chrome.driver","C:\\Users\\mayakaushik\\Documents\\chromedriver.exe");
+            driver=new ChromeDriver();
+        }
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
-    }
-    public void TearDown(){
-        driver.quit();
+        driver.get(prop.getProperty("url"));
     }
 
 }
